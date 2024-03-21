25bad416ccc03047a5655cf448d96e0279faa460
RezaKharisma <rezakharisma1@gmail.com>
2024-03-21 09:43:01 +0800

    Test

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,9 @@ use Laravel\Fortify\Fortify;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
+| test pull
 */
+
 
 // Login
 Fortify::loginView('/', function(){ return view('auth.login'); });
